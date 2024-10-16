87b8b799a9617223e24059cf77b9c73aa91fbd14
timniklas <accounts@tnkoch.de>
2024-10-16 21:56:26 +0200

    Update config_flow.py

--- custom_components/blitzerde/config_flow.py
+++ custom_components/blitzerde/config_flow.py
@@ -17,19 +17,6 @@ class SmartmeConfigFlow(ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         # Specify items in the order they are to be displayed in the UI
         data_schema = {
-            vol.Required("username"): str,
-            vol.Required("password"): str,
-            # Items can be grouped by collapsible sections
-            "ssl_options": section(
-                vol.Schema(
-                    {
-                        vol.Required("abc1", default=True): bool,
-                        vol.Required("abc2", default=True): bool,
-                    }
-                ),
-                # Whether or not the section is initially collapsed (default = False)
-                {"collapsed": False},
-            ),
             "select_location": selector({
                 "location": {}
             })
