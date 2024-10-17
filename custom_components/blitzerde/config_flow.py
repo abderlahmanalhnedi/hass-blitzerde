faee87648fa75dd2d0d9b473062663d79e273cb0
timniklas <accounts@tnkoch.de>
2024-10-17 14:57:17 +0200

    Update config_flow.py

--- custom_components/blitzerde/config_flow.py
+++ custom_components/blitzerde/config_flow.py
@@ -5,7 +5,6 @@ from homeassistant.config_entries import ConfigFlow
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from aiohttp import ClientError, ClientResponseError, ClientSession
 
-from homeassistant.data_entry_flow import section
 from homeassistant.helpers.selector import selector
 
 from .const import DOMAIN
