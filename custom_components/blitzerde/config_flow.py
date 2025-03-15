1924535f8856a38d7351a6c8d13d1aa0185eee5d
timniklas <accounts@tnkoch.de>
2025-03-15 20:54:11 +0100

    clarified config flow

--- custom_components/blitzerde/config_flow.py
+++ custom_components/blitzerde/config_flow.py
@@ -8,6 +8,7 @@ from homeassistant.config_entries import (
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from aiohttp import ClientError, ClientResponseError, ClientSession
 from homeassistant.core import callback
+from homeassistant.data_entry_flow import section
 from homeassistant.helpers.selector import selector
 
 from .const import DOMAIN
@@ -28,20 +29,32 @@ class BlitzerdeConfigFlow(ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            if user_input == {}: #default location
+            if CONF_LOCATION not in user_input: #default location
                 return self.async_abort(reason="location")
 
-            return self.async_create_entry(title=f"Blitzer.de {user_input[CONF_NAME]}", data=user_input)
+            return self.async_create_entry(title=f"Blitzer.de {user_input[CONF_NAME]}", data={
+                CONF_NAME: user_input[CONF_NAME],
+                CONF_LOCATION: user_input[CONF_LOCATION],
+                CONF_SELECTOR: user_input['optional'][CONF_SELECTOR]
+            })
 
         data_schema = {
-            vol.Required(CONF_NAME): str,
-            vol.Required(CONF_SELECTOR, default="Stadt1|Stadt2"): str
+            vol.Required(CONF_NAME): str
         }
         data_schema[CONF_LOCATION] = selector({
             "location": {
                 "radius": True
             }
         })
+        data_schema[vol.Required('optional')] = section(
+            vol.Schema(
+                {
+                    vol.Required(CONF_SELECTOR, default=".*"): str
+                }
+            ),
+            # Whether or not the section is initially collapsed (default = False)
+            {"collapsed": True},
+        )
         return self.async_show_form(step_id="user", data_schema=vol.Schema(data_schema))
 
     @staticmethod
@@ -62,16 +75,34 @@ class BlitzerdeOptionsFlow(OptionsFlowWithConfigEntry):
 
         if user_input is not None:
             # Update config entry with data from user input
-            user_input[CONF_NAME] = self.config_entry.data.get(CONF_NAME)
-            user_input[CONF_LOCATION] = self.config_entry.data.get(CONF_LOCATION)
+            if CONF_LOCATION not in user_input: #default location
+                user_input[CONF_LOCATION] = self.config_entry.data.get(CONF_LOCATION)
+
+            data = {
+                CONF_NAME: self.config_entry.data.get(CONF_NAME),
+                CONF_LOCATION: user_input[CONF_LOCATION],
+                CONF_SELECTOR: user_input['optional'][CONF_SELECTOR]
+            }
             self.hass.config_entries.async_update_entry(
-                self._config_entry, data=user_input
+                self._config_entry, data=data
             )
             return self.async_create_entry(
-                title=self._config_entry.title, data=user_input
+                title=self._config_entry.title, data=data
             )
 
-        data_schema = {
-            vol.Required(CONF_SELECTOR, default=self.config_entry.data.get(CONF_SELECTOR)): str
-        }
+        data_schema = {}
+        data_schema[CONF_LOCATION] = selector({
+            "location": {
+                "radius": True
+            }
+        })
+        data_schema[vol.Required('optional')] = section(
+            vol.Schema(
+                {
+                    vol.Required(CONF_SELECTOR, default=self.config_entry.data.get(CONF_SELECTOR)): str
+                }
+            ),
+            # Whether or not the section is initially collapsed (default = False)
+            {"collapsed": True},
+        )
         return self.async_show_form(step_id="init", data_schema=vol.Schema(data_schema))
