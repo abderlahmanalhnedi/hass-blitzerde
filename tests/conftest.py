d97b9c3eed662bbc302ca66464b1e9099a2767cf
Abdelrahman Al Hnedi <90635282+abderlahmanalhnedi@users.noreply.github.com>
2026-09-08 17:49:44 +0200

    test: rely on auto-registered Home Assistant pytest plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,6 @@ from __future__ import annotations
 
 import pytest
 
-pytest_plugins = "pytest_homeassistant_custom_component.plugins"
-
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(
